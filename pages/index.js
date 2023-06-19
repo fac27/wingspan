4ba17f0061ba6a6e9e51a509b8f09e8726068e86
Zakaria Yaris <info@zakariya.me>
2023-06-19 15:09:24 +0100

    initial folder/file structure

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Wingspan</title>
+      </Head>
+
+      <p>Hello, Nextjs</p>
+    </>
+  );
+}
