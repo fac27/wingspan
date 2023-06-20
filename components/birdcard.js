4136154ca7f2ab93075d377ff46f0317dc2f3f28
cameochoquer <cjchoquer@gmail.com>
2023-06-20 11:23:16 +0100

    husky is doing stuff?

--- components/birdcard.js
+++ components/birdcard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Birdcard() {
   return (
     <div>
-      <Image src="some file path" alt="bird image"/>
+      <Image src="some file path" alt="bird image" />
       <h2>bird name</h2>
     </div>
   );
