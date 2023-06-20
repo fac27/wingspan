6c221123126fdbee5911baca03e5235dd29e0e36
cameochoquer <cjchoquer@gmail.com>
2023-06-20 14:28:49 +0100

    na

--- components/birdcard.js
+++ components/birdcard.js
@@ -4,10 +4,13 @@ export default function BirdCard({ tempStyles, allBirdsData }) {
   return (
     <div style={tempStyles}>
       <h2>Flamingo</h2>
-      <Image 
-      // src={allBirdsData.img_path} 
-      src='public/images/American_Kestrel.png'
-      alt="bird image" width={123} height={123} />
+      <Image
+        // src={allBirdsData.img_path}
+        src="public/images/American_Kestrel.png"
+        alt="bird image"
+        width={123}
+        height={123}
+      />
       <p>Wingspan</p>
       <p>Habitat</p>
       <p></p>
