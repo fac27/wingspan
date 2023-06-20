fee8e537ce60259d5fba0d1d1e20eff8082de80a
cameochoquer <cjchoquer@gmail.com>
2023-06-20 14:19:13 +0100

    adding images

--- components/birdcard.js
+++ components/birdcard.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
-export default function BirdCard({ tempStyles }) {
+export default function BirdCard({ tempStyles, allBirdsData }) {
   return (
     <div style={tempStyles}>
       <h2>Flamingo</h2>
-      <Image src="/someBirdImage" alt="bird image" width={123} height={123} />
+      <Image 
+      // src={allBirdsData.img_path} 
+      src='public/images/American_Kestrel.png'
+      alt="bird image" width={123} height={123} />
       <p>Wingspan</p>
       <p>Habitat</p>
       <p></p>
