6bb31af0c5b3f888ef0138285710cce30ad682bb
eliazzo <bethazz@hotmail.co.uk>
2023-06-21 15:02:43 +0100

    cypress

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,19 +1,19 @@
-const { appendMutableCookies } = require("next/dist/server/web/spec-extension/adapters/request-cookies")
+const {
+  appendMutableCookies,
+} = require("next/dist/server/web/spec-extension/adapters/request-cookies");
 
-describe('test wingspan', () => {
-  it('takes user to homepage', () => {
-    cy.visit('http://localhost:3000/')
-  })
-})
+describe("test wingspan", () => {
+  it("takes user to homepage", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
 
 // To run the test:
 
 // Make sure development server is running:
 // npm run dev
 
-
 // Open cypress web app
 // npx cypress open
 
 // Run Tests
-
