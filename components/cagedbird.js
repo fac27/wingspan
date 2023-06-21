a80d2eb5b7213f2a1b32a661ed6183f691375add
Taha <tahafhassan@gmail.com>
2023-06-21 12:23:45 +0100

    total wingspan rendering
    added wingspan to cagedBirdcomponent

--- components/cagedbird.js
+++ components/cagedbird.js
@@ -5,6 +5,7 @@ export default function CagedBird({ handleAdd, bird, handleRemove }) {
     <div>
       <h2>{bird.name}</h2>
       <Image src={bird.img_path} alt="bird image" width={123} height={123} />
+      <p>Wingspan: {bird.wingspan}</p>
       <p>
         <strong>Quantity: {bird.quantity}</strong>
       </p>
