289ce1eebcc08118f8225e55eb0a6b3f7da44a3d
cameochoquer <cjchoquer@gmail.com>
2023-06-21 12:29:01 +0100

    feat: filtering works but needs to move?

--- components/viewBirds.js
+++ components/viewBirds.js
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import BirdCard from "./birdcard.js";
 import { styled } from "styled-components";
 
 export default function ViewBirds({ handleAdd, allBirdsData }) {
   const uniqueHabitats = [...new Set(allBirdsData.map((bird) => bird.habitat))];
+  const [selectedHabitat, setSelectedHabitat] = useState(null);
+    
+  const handleClick = (habitat) => {
+    setSelectedHabitat(habitat);
+  };
+
+  const filteredBirds = selectedHabitat 
+    ? allBirdsData.filter(bird => bird.habitat === selectedHabitat) 
+    : allBirdsData;
 
   return (
     <>
       <form>
         {uniqueHabitats.map((habitat) => (
-          <button key={habitat} onclick={handleClick} type="button">
+          <button key={habitat} onClick={() => handleClick(habitat)} type="button">
             {habitat}
           </button>
         ))}
       </form>
       <BirdContainer>
-        {allBirdsData.map((bird) => (
+        {filteredBirds.map((bird) => (
           <BirdCard handleAdd={handleAdd} key={bird.id} bird={bird}></BirdCard>
         ))}
       </BirdContainer>
