50e0a20b69339bf475b793fa66ac3d717a4a4081
cameochoquer <cjchoquer@gmail.com>
2023-06-21 12:29:28 +0100

    husky fix

--- components/viewBirds.js
+++ components/viewBirds.js
@@ -5,20 +5,24 @@ import { styled } from "styled-components";
 export default function ViewBirds({ handleAdd, allBirdsData }) {
   const uniqueHabitats = [...new Set(allBirdsData.map((bird) => bird.habitat))];
   const [selectedHabitat, setSelectedHabitat] = useState(null);
-    
+
   const handleClick = (habitat) => {
     setSelectedHabitat(habitat);
   };
 
-  const filteredBirds = selectedHabitat 
-    ? allBirdsData.filter(bird => bird.habitat === selectedHabitat) 
+  const filteredBirds = selectedHabitat
+    ? allBirdsData.filter((bird) => bird.habitat === selectedHabitat)
     : allBirdsData;
 
   return (
     <>
       <form>
         {uniqueHabitats.map((habitat) => (
-          <button key={habitat} onClick={() => handleClick(habitat)} type="button">
+          <button
+            key={habitat}
+            onClick={() => handleClick(habitat)}
+            type="button"
+          >
             {habitat}
           </button>
         ))}
